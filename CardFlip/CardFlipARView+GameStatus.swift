9acd1ef611939fa4df3332236efb1d9424113a85
Max Cobb <5754073+maxxfrazer@users.noreply.github.com>
2019-07-31 18:46:33 +0100

    moved gameStatus code and ARSession relevant code to their own files

---
+++ CardFlip/CardFlipARView+GameStatus.swift
@@ -0,0 +1,52 @@
+//
+//  CardFlipARView+GameStatus.swift
+//  CardFlip
+//
+//  Created by Max Cobb on 7/31/19.
+//  Copyright © 2019 Max Cobb. All rights reserved.
+//
+
+import RealityKit
+
+enum GameStatus {
+  case planeSearching
+  case positioning
+  case playing
+}
+
+extension CardFlipARView {
+
+  func changedFromPositioningStatus() {
+    self.flipTable?.components[CollisionComponent] = nil
+    self.confirmButton?.removeFromParent()
+    self.installedGestures = self.installedGestures.filter({ (recogniser) -> Bool in
+      recogniser.isEnabled = false
+      return false
+    })
+  }
+  func setToPositioningStatus() {
+    guard let table = self.flipTable else {
+      return
+    }
+    table.components[CollisionComponent] = CollisionComponent(shapes: [.generateBox(size: [4, 0.4, 4])])
+    self.installedGestures.append(
+      contentsOf: self.installGestures([.translation, .rotation], for: table)
+    )
+
+    // TODO: - Change the button, maybe change to 2D UIButton?
+    let confirmButton = self.confirmButton ?? ARButton(
+      transform: Transform(
+        scale: [1, 1, 1],
+        rotation: simd_quatf(angle: 0, axis: [1,0,0]),
+        translation: [-0.5, 1.0, 0]),
+      model: ModelComponent(
+        mesh: .generateSphere(radius: 0.4),
+        materials: [SimpleMaterial(color: .green, isMetallic: false)]
+    )) {
+      self.status = .playing
+    }
+    self.confirmButton = confirmButton
+    self.flipTable?.addChild(confirmButton)
+
+  }
+}
--- CardFlip/CardFlipARView.swift
+++ CardFlip/CardFlipARView.swift
@@ -9,26 +9,19 @@
 import RealityKit
 import ARKit
 
-enum GameStatus {
-  case planeSearching
-  case positioning
-  case playing
-}
-
-class CardFlipARView: ARView, ARSessionDelegate {
+class CardFlipARView: ARView {
   let coachingOverlay = ARCoachingOverlayView()
   var tableAdded = false
 
-  // MARK: - Touch Gesture Variables
-  var touchStartedOn: FlipCard? = nil
-  var currentlyFlipped: FlipCard? = nil
-  var canTap = true
-  var flipTable: FlipTable? = nil
-  var confirmButton: ARButton?
-  var installedGestures: [EntityGestureRecognizer] = []
 
   var status: GameStatus = .planeSearching {
     didSet {
+      switch oldValue {
+      case .positioning:
+        changedFromPositioningStatus()
+      default:
+        print("status was: \(status)")
+      }
       switch status {
       case .positioning:
         setToPositioningStatus()
@@ -38,37 +31,13 @@ class CardFlipARView: ARView, ARSessionDelegate {
     }
   }
 
-  func setToPositioningStatus() {
-    guard let table = self.flipTable else {
-      return
-    }
-    table.components[CollisionComponent] = CollisionComponent(shapes: [.generateBox(size: [4, 0.4, 4])])
-    self.installedGestures.append(
-      contentsOf: self.installGestures([.translation, .rotation], for: table)
-    )
-
-    // TODO: - Change the button, maybe change to 2D UIButton?
-    let confirmButton = self.confirmButton ?? ARButton(
-      transform: Transform(
-        scale: [1, 1, 1],
-        rotation: simd_quatf(angle: 0, axis: [1,0,0]),
-        translation: [-0.5, 1.0, 0]),
-      model: ModelComponent(
-        mesh: .generateSphere(radius: 0.4),
-        materials: [SimpleMaterial(color: .green, isMetallic: false)]
-    )) {
-      self.flipTable?.components[CollisionComponent] = nil
-      self.confirmButton?.removeFromParent()
-      self.status = .playing
-      self.installedGestures = self.installedGestures.filter({ (recogniser) -> Bool in
-        recogniser.isEnabled = false
-        return false
-      })
-    }
-    self.confirmButton = confirmButton
-    self.flipTable?.addChild(confirmButton)
-
-  }
+  // MARK: - Touch Gesture Variables
+  var touchStartedOn: FlipCard? = nil
+  var currentlyFlipped: FlipCard? = nil
+  var canTap = true
+  var flipTable: FlipTable? = nil
+  var confirmButton: ARButton?
+  var installedGestures: [EntityGestureRecognizer] = []
 
   /// Add the FlipTable object
   func addFlipTable() {
@@ -82,15 +51,6 @@ class CardFlipARView: ARView, ARSessionDelegate {
       self.scene.anchors.append(flipTable)
     } else {
       print("couldnt make flip table")
-    }
-  }
-  func session(_ session: ARSession, didUpdate frame: ARFrame) {
-    if self.status == .planeSearching {
-      if self.flipTable?.isAnchored ?? false {
-        self.status = .positioning
-
-//        self.status = .playing
-      }
     }
   }
 }
